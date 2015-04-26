c7a338ab944dc75fa7cc1b756a51e0599e9638e3
Matthew Lyon <matthew@lyonheart.us>
2015-04-26 11:29:16 -0700

    Remove unnecessary files

--- farmhouse-theme-autoloads.el
+++
@@ -1,2 +0,0 @@
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-(provide 'farmhouse-theme-autoloads)
--- farmhouse-theme-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package
-  "farmhouse-theme"
-  "1.0"
-  "Farmhouse theme for Emacs"
-  '((emacs "24")))
