1978b5a8d1d9941509d0dd16705d60fbf4df671f
Matthew Lyon <matthew@lyonheart.us>
2015-04-27 23:39:04 -0700

    re-add -pkg, apparently needed after all

---
+++ farmhouse-theme-pkg.el
@@ -0,0 +1,7 @@
+(define-package
+  "farmhouse-theme"
+  "20140427.2338"
+  "Farmhouse Theme, Emacs edition"
+  'nil
+  :url "https://github.com/mattly/emacs-farmhouse-theme"
+  :keywords '("color" "theme"))
